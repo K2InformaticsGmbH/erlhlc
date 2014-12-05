94126dbb402da41d8d9f516779e6ac2a033b4e14
Bikram Chatterjee <razorpeak@gmail.com>
2014-12-05 11:12:29 +0100

    optional physical hb interface

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Parameters:  Number_of_nodes
+# Parameters: [-hb] [Number_of_nodes]
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
@@ -10,8 +10,20 @@ else
     #exename='erl.exe'
 fi
 
+hbhost="127.0.0.1"
+if [[ "$1" == "-hb" ]]; then
+    host=`hostname`
+    hostparts=(${host//./ })
+
+    hbhost=${hostparts[0]}h
+    for (( i=1; i<${#hostparts[@]}; i++ ));
+    do
+        hbhost=$hbhost.${hostparts[$i]}
+    done
+fi
+
 # Node name
-node="erlhlc0@127.0.0.1"
+node="erlhlc0@$hbhost"
 node_name="-name $node"
 
 # Cookie
@@ -36,14 +48,19 @@ echo "------------------------------------------"
 # Starting erlhlc
 $exename $start_opts -s erlhlc
 
-nodes=0
-if [[ "$1" -gt "0" ]]; then
+if [[ "$1" == "-hb" ]]; then
+    nodes=$2
+else
     nodes=$1
+fi
+
+if [[ "$nodes" -le "0" ]]; then
+    nodes=0
 fi
 
 for (( c=1; c<=$nodes; c++ ))
 do
-    node_name="-name erlhlc$c@127.0.0.1"
+    node_name="-name erlhlc$c@$hbhost"
     start_opts="$paths $cookie $node_name"
     $exename $start_opts -s erlhlc -eval "apply(net_adm,ping,['$node'])."
 done
